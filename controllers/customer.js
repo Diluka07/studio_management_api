7a8029e545815928dfd3ec4f305b5c80f80cd726
Diluka Hewage <57040599+Diluka07@users.noreply.github.com>
2023-02-08 07:59:37 +0530

    customer update

--- controllers/customer.js
+++ controllers/customer.js
@@ -6,37 +6,61 @@ const Customer = require("../models/Customer");
 // @route   POST /api/customers
 // @access  Private/Admin
 exports.createCustomer = asyncHandler(async (req, res, next) => {
-    const customer = await Customer.create(req.body);
-  
-    res.status(201).json({
-      success: true,
-      data: customer,
-    });
+  const customer = await Customer.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: customer,
   });
-  
-  // @desc    Update Customer
-  // @route   PUT /api/customers/:id
-  // @access  Private/Admin
-  exports.updateCustomer = asyncHandler(async (req, res, next) => {
-    const customer = await Customer.findByIdAndUpdate(req.params.id, req.body, {
+});
+
+// @desc    Update Customer
+// @route   PUT /api/customers/:id
+// @access  Private/Admin
+exports.updateCustomer = asyncHandler(async (req, res, next) => {
+  console.log(req.body);
+  const customer = await Customer.findByIdAndUpdate(
+    req.body.id,
+    {
+      nic: req.body.nic,
+      name: req.body.name,
+      contactNumber1: req.body.contactNumber1,
+      contactNumber2: req.body.contactNumber2,
+      address: req.body.address,
+    },
+    {
       new: true,
       runValidators: true,
-    });
-  
-    res.status(201).json({
-      success: true,
-      data: customer,
-    });
-  });
+    }
+  );
 
-  // @desc    Get All Customers
-  // @route   GET /api/customers
-  // @access  Public
-  exports.getAllCustomers = asyncHandler(async (req, res, next) => {
-    const customers = await Customer.find();
-  
-    res.status(200).json({
-      success: true,
-      data: customers,
-    });
+  res.status(201).json({
+    success: true,
+    data: customer,
   });
+});
+
+// @desc    Get All Customers
+// @route   GET /api/customers
+// @access  Public
+exports.getAllCustomers = asyncHandler(async (req, res, next) => {
+  const customers = await Customer.find();
+
+  res.status(200).json({
+    success: true,
+    data: customers,
+  });
+});
+
+// @desc    Get single customer
+// @route   GET /api/customers/:id
+// @access  Private
+exports.getSingleCustomer = asyncHandler(async (req, res, next) => {
+  const customer = await Customer.findById(req.params.id);
+  if (!customer) {
+    return next(
+      new ErrorResponse(`No customer found with id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({ success: true, data: customer });
+});
--- routes/customers.js
+++ routes/customers.js
@@ -3,14 +3,16 @@ const {
   createCustomer,
   updateCustomer,
   getAllCustomers,
+  getSingleCustomer,
 } = require("../controllers/customer");
 const router = express.Router();
 const { protect, authorize } = require("../middleware/auth");
 
-router.use(protect);
-router.use(authorize("manager"));
-
-router.route("/").get(getAllCustomers).post(createCustomer);
-router.route("/:id").put(updateCustomer);
+router
+  .route("/")
+  .get(protect, authorize("manager", "cashier"), getAllCustomers)
+  .post(protect, authorize("manager"), createCustomer)
+  .put(protect, authorize("manager"), updateCustomer);
+router.route("/:id").get(protect, authorize("manager"), getSingleCustomer);
 
 module.exports = router;
